c9860d867c9a9e4cfa24036862a115db13c6216d
Ruomoe <782963325@qq.com>
2022-03-10 17:04:51 +0800

    1.0.4 fix papi null

--- src/main/java/cc/canyi/qilingsuit/utils/PluginUtils.java
+++ src/main/java/cc/canyi/qilingsuit/utils/PluginUtils.java
@@ -3,6 +3,8 @@ package cc.canyi.qilingsuit.utils;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Arrays;
+
 public class PluginUtils {
     /**
      * 判断插件是否存在
@@ -10,7 +12,8 @@ public class PluginUtils {
      * @return 是否存在
      */
     public static boolean pluginIsActive(String pluginName) {
-        return Bukkit.getPluginManager().getPlugin(pluginName) != null;
+        return Arrays.stream(Bukkit.getPluginManager().getPlugins()).anyMatch(plugin -> plugin.getName().equals(pluginName));
+//        return Bukkit.getPluginManager().getPlugin(pluginName) != null;
     }
 
     public static Plugin getPlugin(String pluginName) {
